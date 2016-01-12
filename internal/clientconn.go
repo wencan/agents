dbf344eec9ef7c275e689104a958420a6ba74892
wencan <i@wencan.org>
2016-01-12 11:21:15 +0800

    补上clientconn.go

---
+++ internal/clientconn.go
@@ -0,0 +1,61 @@
+package internal
+
+import (
+	"./../agent"
+	"google.golang.org/grpc"
+	"golang.org/x/net/context"
+	"sync/atomic"
+)
+
+type ClientConn struct {
+	agent.AgentClient
+	conn *grpc.ClientConn
+
+	ref *int32	//shared reference counter
+}
+
+func Dial(target string, opts ...grpc.DialOption) (cc *ClientConn, err error) {
+	var conn *grpc.ClientConn
+	conn, err = grpc.Dial(target, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	raw := agent.NewAgentClient(conn)
+
+	var ref int32 = 1
+
+	cc = &ClientConn{
+		AgentClient: raw,
+		conn: conn,
+		ref: &ref,
+	}
+
+	return cc, nil
+}
+
+func (cc *ClientConn) Fork() *ClientConn {
+	atomic.AddInt32(cc.ref, 1)
+	return &ClientConn{
+		AgentClient: cc.AgentClient,
+		conn: cc.conn,
+		ref: cc.ref,
+	}
+}
+
+func (cc *ClientConn) State() (grpc.ConnectivityState, error) {
+	return cc.conn.State()
+}
+
+func (cc *ClientConn) WaitForStateChange(ctx context.Context, sourceState grpc.ConnectivityState) (grpc.ConnectivityState, error) {
+	return cc.conn.WaitForStateChange(ctx, sourceState)
+}
+
+func (client *ClientConn) Close() (err error) {
+	ref := atomic.AddInt32(client.ref, -1)
+	if ref == 0 {
+		return client.conn.Close()
+	}
+
+	return nil
+}
